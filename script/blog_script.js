2a26030da8cf496ad684f4c159216e5a3f37d2ee
rianhasansiam <rianhasan1971@gmail.com>
2024-09-23 23:40:10 +0600

    file path change

--- script/blog_script.js
+++ script/blog_script.js
@@ -1,4 +1,4 @@
 const homeSwitchBtn=document.getElementById('homeSwitchBtn')
 homeSwitchBtn.addEventListener('click', function(){
-    window.location.href=('../home.html')
+    window.location.href=('../index.html')
   })
